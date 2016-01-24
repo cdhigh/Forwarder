ac8175a7bc922eb6fa500a9f4aef9bd5e8dc7df6
cdhigh <chsqyuan@hotmail.com>
2016-01-24 11:16:13 -0200

    v1.1
    增加heroic需要的Profile

--- readme.txt
+++ readme.txt
@@ -1,11 +1,11 @@
 ﻿这是一个很简单很简单的HTTP请求转发服务器，代码只有40行，可以很简单的部署到任何支持WSGI的
 云端服务器，然后使用其中转KindleEar的个别HTTP请求，以便绕过墙或绕过部分网站对GAE的IP的封锁。
 
-作者已经将其部署到appfog上，目前免费提供服务，但不保证一直服务，如果后续流量很大，可能会修改。
-http://forwarder.ap01.aws.af.cm
+作者已经将其部署到Heroku上，目前免费提供服务，但不保证一直服务，如果后续流量很大，可能会修改。
+http://kforwarder.herokuapp.com/
 
 使用方法：
 将要转发到URL转换成如下格式：
-http://forwarder.ap01.aws.af.cm/?k=xzSlE&t=timeout&u=URL
+http://kforwarder.herokuapp.com/?k=xzSlE&t=timeout&u=URL
 其中xzS1E为验证码，timeout为超时时间，可省略，默认为30s，URL则为你自己的URL
 例子参照KindleEar项目的books/ZhihuDaily.py (知乎日报)
--- wsgi.py
+++ wsgi.py
@@ -2,21 +2,21 @@
 # -*- coding:utf-8 -*-
 """ python网络请求转发器，可以将其部署到任何一个支持WSGI的托管空间。
 用法：
-  http://hostedurl?u=url&k=AUTHKEY&t=timeout
+  http://hostedurl?k=AUTHKEY&t=timeout&u=url
   解析：
   hostedurl: 你搭建的转发服务器的URL
-  url: 需要转发到url，需要先使用urllib.quote转义，特别是如果有&符号
+  url: 需要转发的url，需要先使用urllib.quote转义，特别是如果有&符号
   AUTHKEY: 为了防止滥用，需要提供一个key，为ALLOW_KEYS里面的任何一个值
   timeout: [可选]超时时间，默认为30s
  """
 
-__Version__ = "1.0"
-__Author__ = "Arroz"
+__Version__ = "1.1"
+__Author__ = "Arroz <https://github.com/cdhigh>"
 
 from wsgiref.util import is_hop_by_hop
 import urllib, urllib2, socket, bottle
 
-ALLOW_KEYS = ('xzSlE','ILbou','DukPL')
+ALLOW_KEYS = ('xzSlE','ILbou')
 
 application = app = bottle.Bottle()
 
@@ -24,7 +24,7 @@ application = app = bottle.Bottle()
 def Home():
     resp = bottle.response
     qry = bottle.request.query
-    url,k,timeout = qry.u, qry.k, int(qry.get('t','30'))
+    url, k, timeout = qry.u, qry.k, int(qry.get('t', '30'))
     if k and k not in ALLOW_KEYS:
         return 'Auth Key is invalid!'
     
@@ -34,7 +34,7 @@ def Home():
             req = urllib2.Request(url)
             req.add_header('User-Agent', "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)")
             req.add_header('Accept', "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-            ret = urllib2.urlopen(req,timeout=timeout)
+            ret = urllib2.urlopen(req, timeout=timeout)
             content = ret.read()
             headers = [(n,v) for n,v in ret.info().items() if not is_hop_by_hop(n)]
             cookieadded = False
@@ -49,10 +49,11 @@ def Home():
         except socket.timeout:
             pass
         except Exception as e:
-            print("ERR:%s:%s" % (type(e),str(e)))
+            print("ERR : %s : %s" % (type(e), str(e)))
             bottle.abort(400)
     else:
-        return "<html><head><title>Forwarder Url</title></head><body>Forwarder : thisurl?u=url&k=AUTHKEY&t=timeout</body></html>"
+        return "<html><head><title>Forwarder Url</title></head><body>Forwarder : thisurl?k=AUTHKEY&t=timeout&u=url</body></html>"
 
-if __name__ == '__main__': #for debug
-    bottle.run(app, reloader=True)
+if __name__ == '__main__':
+    #bottle.run(app, reloader=True) #for debug in computer
+    bottle.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000))) #for Heroku
