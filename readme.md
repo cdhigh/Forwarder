c2a324f8708fc56eac4914b89e2478c651759f7a
cdhigh <cdhigh@users.noreply.github.com>
2024-08-14 19:37:02 -0300

    v1.4

--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 # 描述：  
 
-这是一个很简单的HTTP请求转发服务器，代码只有40行，没有任何外部依赖，可以很简单的部署到任何支持Python WSGI的
+这是一个很简单的HTTP请求转发服务器，没有任何外部依赖，可以很简单的部署到任何支持Python WSGI的
 云端服务器，或部署为cloudflare的worker。   
 用于中转KindleEar的个别HTTP请求，以便绕过墙或绕过部分网站对GAE的IP的封锁。   
 
 # 使用方法：   
 在KindleEar的recipe文件里面将需要转发的feed地址修改为以下的调用格式：   
-`http://example.com/?k=xzSlE&t=timeout&u=URL`    
-其中xzS1E为验证码，timeout为超时时间，可省略，默认为30s，URL则为要转发的URL.   
+`http://example.com/?k=xzSlE&t=TIMEOUT&u=URL`    
+其中xzS1E为验证码，TIMEOUT为超时时间，可省略，默认为30s，URL则为要转发的URL.   
 
 # 一键部署到Vercel：   
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fcdhigh%2FForwarder&env=ALLOW_KEY)
@@ -15,7 +15,7 @@
 # 部署到cloudflare：
 1. 在cloudflare的dashboard页面点击左侧的 `Workers & Pages` 导航栏。   
 2. 点击 `Create Worker`，输入一个域名，点击 `Deploy`。   
-3. 部署完成后点击对应Worker右上角的 `Edit Code` ，将 `cloudflare_workers.js` 的内容拷贝粘贴过去，再点击右上角的 `Deploy`，完成部署。   
+3. 部署完成后点击对应Worker右上角的 `Edit Code` ，将 `cloudflare_worker.js` 的内容拷贝粘贴过去，再点击右上角的 `Deploy`，完成部署。   
 
 # 部署到Heroku：
 参见 [DeployToHeroku.md](https://github.com/cdhigh/forwarder/blob/master/DeployToHeroku.md)
--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-""" python网络请求转发器，可以轻易将其部署到任何一个支持WSGI的托管空间，兼容Python2/3。    
+""" python网络请求转发器，没有任何外部依赖，可以轻易将其部署到任何一个支持WSGI的托管空间，兼容Python2/3。    
 用法：
-  http://hostedurl?k=AUTHKEY&t=timeout&u=url
+  http://hostedurl?k=AUTHKEY&t=TIMEOUT&u=URL
   解析：
-  hostedurl: 你搭建的转发服务器的URL
-  url: 需要转发的url，需要先使用urllib.quote转义，特别是如果有&符号
+  hostedurl: 转发服务器的URL
+  URL: 需要转发的url，需要先使用urllib.quote转义，特别是如果有&符号
   AUTHKEY: 为了防止滥用，需要提供一个key
-  timeout: [可选]超时时间，默认为30s
- """
-
-__Version__ = "1.3"
-__Author__ = "cdhigh <https://github.com/cdhigh>"
-
+  TIMEOUT: [可选]超时时间，默认为30s
+"""
+import os, socket
+from wsgiref.util import is_hop_by_hop
 try:
     from urllib.parse import unquote, urlparse
     from urllib.request import Request, urlopen
@@ -24,52 +22,65 @@ except ImportError:
     urlopen = urllib2.urlopen
     URLError = urllib2.URLError
 
-from wsgiref.util import is_hop_by_hop
-import os, socket, bottle
-
 ALLOW_KEY = os.environ.get('ALLOW_KEY') or 'xzSlE'
+DEFAULT_TIMEOUT = 30
+__Version__ = "1.4"
 
-app = bottle.Bottle()
-application = app
+def msg(txt):
+    return "<html><head><title>Url Forwarder</title></head><body><h3>Url Forwarder v{}</h3>{}</body></html>".format(__Version__, txt).encode('utf-8')
 
-@app.route(r'/')
-def Home():
-    resp = bottle.response
-    qry = bottle.request.query
-    url, k, timeout = qry.u, qry.k, int(qry.get('t', '30'))
-    if k and k != ALLOW_KEY:
-        return 'Auth Key is invalid!'
+def application(environ, start_response):
+    #from wsgiref.util import setup_testing_defaults
+    #setup_testing_defaults(environ)
     
-    if url and k:
-        url = unquote(url).replace(' ', r'%20')
-        parts = urlparse(url)
-        referer = '%s://%s' % (parts.scheme, parts.netloc)
-        try:
-            req = Request(url)
-            req.add_header('User-Agent', "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)")
-            req.add_header('Accept', "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-            req.add_header('Accept-Language', "en,*")
-            req.add_header('Referer', referer)
-            ret = urlopen(req, timeout=timeout)
-            content = ret.read()
-            headers = [(n, v) for n, v in ret.info().items() if not is_hop_by_hop(n)]
-            cookieAdded = False
-            for n, v in headers:
-                if n == 'Set-Cookie' and cookieAdded:
-                    resp.add_header(n, v)
-                else:
-                    resp.set_header(n, v)
-                    if n == 'Set-Cookie':
-                        cookieAdded = True
-            return content
-        except socket.timeout:
-            pass
-        except Exception as e:
-            print("ERR : %s : %s" % (type(e), str(e)))
-            bottle.abort(400)
-    else:
-        return "<html><head><title>Forwarder Url</title></head><body>Forwarder(%s) : thisurl?k=AUTHKEY&t=timeout&u=url</body></html>" % __Version__
+    # Parse query parameters
+    query_string = environ.get('QUERY_STRING', '')
+    query_params = dict(qc.split("=", 1) for qc in query_string.split("&") if "=" in qc)
+    
+    url = query_params.get('u')
+    key = query_params.get('k')
+    try:
+        timeout = int(query_params.get('t', DEFAULT_TIMEOUT))
+    except:
+        timeout = DEFAULT_TIMEOUT
+    
+    if key and key != ALLOW_KEY:
+        start_response('403 FORBIDDEN', [('Content-Type', 'text/html')])
+        return [msg('Auth Key is invalid!')]
+    
+    if not url or not key:
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return [msg('<strong>Usage: </strong>thisurl?k=AUTHKEY&t=TIMEOUT&u=URL')]
+
+    url = unquote(url).replace(' ', r'%20')
+    parts = urlparse(url)
+    referer = f'{parts.scheme}://{parts.netloc}'
+    
+    try:
+        req = Request(url)
+        req.add_header('User-Agent', "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)")
+        req.add_header('Accept', "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+        req.add_header('Accept-Language', "en,*")
+        req.add_header('Referer', referer)
+        ret = urlopen(req, timeout=timeout)
+        content = ret.read()
+        headers = [(n, v) for n, v in ret.info().items() if not is_hop_by_hop(n)]
+        start_response('200 OK', headers)
+        return [content]
+    except socket.timeout:
+        start_response('504 GATEWAY TIMEOUT', [('Content-Type', 'text/html')])
+        return [msg('Timeout error')]
+    except Exception as e:
+        err_msg = str(e).replace("<", "&lt;").replace(">", "&gt;")
+        print("ERROR : {} : {}".format(type(e), str(e)))
+        start_response('400 BAD REQUEST', [('Content-Type', 'text/html')])
+        return [msg('Error occurred: <br/>{}'.format(err_msg))]
+    
+app = application
 
 if __name__ == '__main__':
-    #bottle.run(reloader=True) #for debug in computer
-    bottle.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000))) #for Heroku
+    from wsgiref.simple_server import make_server
+    port = int(os.environ.get('PORT', 5000))
+    with make_server('0.0.0.0', port, application) as httpd:
+        print("Serving on port {}...".format(port))
+        httpd.serve_forever()
